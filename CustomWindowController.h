cd17867383839855f1e0d3d2203db0539cb3a805
Jan <github@geheimwerk.de>
2011-10-02 13:16:35 +0200

    Adding CustomWindowController.

---
+++ CustomWindowController.h
@@ -0,0 +1,16 @@
+//
+//  CustomWindowController.h
+//  CustomTextViewDemo
+//
+//  Created by Jan on 02.10.11.
+//  Copyright 2011 geheimwerk.de. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CustomWindowController : NSWindowController {
+
+}
+
+@end
---
+++ CustomWindowController.m
@@ -0,0 +1,14 @@
+//
+//  CustomWindowController.m
+//  CustomTextViewDemo
+//
+//  Created by Jan on 02.10.11.
+//  Copyright 2011 geheimwerk.de. All rights reserved.
+//
+
+#import "CustomWindowController.h"
+
+
+@implementation CustomWindowController
+
+@end
